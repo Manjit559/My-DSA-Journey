d48d5adb76d09454e5eb66983bc678f2d0e2e3ec
Manjit kumar <167091085+Manjit559@users.noreply.github.com>
2025-08-01 13:09:45 +0530

    Add files via upload

---
+++ Array a “/removeDuplicate.java
@@ -0,0 +1,26 @@
+public class removeDuplicate{
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0; 
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
+        int len = removeDuplicates(arr);
+
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < len; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
