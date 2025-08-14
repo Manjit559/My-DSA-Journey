a19a00d031ff17fd72410e9d2bbba25688347115
Manjit kumar <167091085+Manjit559@users.noreply.github.com>
2025-08-14 19:27:36 +0530

    Update Intersection.java

--- Array a “/Intersection.java
+++ Array a “/Intersection.java
@@ -1,1 +1,19 @@
 
+import java.util.*;
+
+public class Intersection {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 2, 3, 4};
+        int[] arr2 = {2, 2, 4, 6};
+
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : arr1) set1.add(num);
+
+        Set<Integer> intersection = new HashSet<>();
+        for (int num : arr2) {
+            if (set1.contains(num)) intersection.add(num);
+        }
+
+        System.out.println(intersection);
+    }
+}
