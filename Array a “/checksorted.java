3f478b6de56562799c737d74732f66e483184c3d
Manjit kumar <167091085+Manjit559@users.noreply.github.com>
2025-08-23 20:55:00 +0530

    Create checksorted.java

---
+++ Array a “/checksorted.java
@@ -0,0 +1,18 @@
+public class CheckSorted {
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 6, 8};  
+        if (isSorted(arr)) {
+            System.out.println("The array is sorted.");
+        } else {
+            System.out.println("The array is not sorted.");
+        }
+    }
+}
