378ffcd10cad77fa4daecfa5ab0dd1802fe186a9
Manjit.. <167091085+Manjit559@users.noreply.github.com>
2025-10-28 20:52:50 +0530

    Update arithmeticoperations.java

--- Array a “/arithmeticoperations.java
+++ Array a “/arithmeticoperations.java
@@ -1,1 +1,19 @@
+package practice_java;
+
+public class arithmeticoperations {
+    public static void main(String[] args) {
+  
+int x=10;
+int y=x++;
+System.out.println(++x);
+System.out.println(x);
+System.out.println(y);
+int z=4;
+System.out.println(z);
+double a=90;
+System.out.println(a);
+System.out.println(x);
+   
+}
+}
 
