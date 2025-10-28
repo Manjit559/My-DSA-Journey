cd183d7e6a0277833fbd7a48ef7310bad3a3e824
Manjit.. <167091085+Manjit559@users.noreply.github.com>
2025-10-28 20:50:34 +0530

    Update profitloss.java

--- Array a “/profitloss.java
+++ Array a “/profitloss.java
@@ -1,1 +1,23 @@
+package practice_java;
+import java.util.Scanner;
+public class profitloss {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter cp:");
+      int cp = sc.nextInt();
+      System.out.println("Enter sp:");
+      int sp = sc.nextInt();
+      if (sp >= cp) // cp = cost price
+         System.out.println("profit");
+
+      System.out.println("Loss");
+      if (cp >= sp) // sp = selling price
+
+         System.out.println("no profit no loss");
+      if (sp == cp)
+         ;
+      sc.close();
+   }
+
+}
 
